373c20beb802204be1dc50e03935f3d4289650b9
@dovganss <dovganss@gmail.com>
2021-10-19 09:41:22 +0300

    feat: add form-input and form-label

---
+++ src/components/atomes/form-input/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function FormInput() {
+  return <inpu className="form__input" type="string"></inpu>;
+}
+
+export default FormInput;
---
+++ src/components/atomes/form-label/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css'
+
+function FormLabel() {
+  return (
+    <label>
+      <p className="form__label">User name</p>
+    </label>
+  );
+}
+
+export default FormLabel;
